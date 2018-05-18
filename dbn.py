8e96ccfd4f6d13ab7312241574ec3cde0053ea4f
lizhongguo <15510978399lzg@gmail.com>
2018-05-18 14:24:49 +0800

    rewrite code of rbm

--- dbn.py
+++ dbn.py
@@ -2,26 +2,45 @@ import numpy as np
 import idx2numpy
 import Image
 import pickle
+import os
 
-images=idx2numpy.convert_from_file("train-images-idx3-ubyte")
-data=[]
-temp=[]
-for image in images:
-    for i in image:
-        for j in i:
-            temp.append(j)
-    data.append(temp)
+if not os.path.exists('X.bin'):
+    images=idx2numpy.convert_from_file("train-images-idx3-ubyte")
+    Y = idx2numpy.convert_from_file("train-labels-idx1-ubyte")
+
+    data=[]
     temp=[]
-X = np.asarray(data , 'float32')
-#print X.shape
-#X = (X-np.min(X,0))/(np.max(X,0)+0.0001)
-X = X/255.0
+    index = 0;
+    for image , label in zip(images, Y):
+        index = index + 1
+        for i in image:
+            for j in i:
+                temp.append(j/255.0)
+
+        for i in range(10):
+            if(i != label):
+                temp.append(0.0)
+            elif(index > 50000):
+                temp.append(0.0)
+            else:
+                temp.append(1.0)
+        data.append(temp)
+        temp=[]
+
+    X = np.asarray(data , 'float32')
+    f_x = open('X.bin','wb')
+    pickle.dump(X,f_x)
+else:
+    f_x = open('X.bin','rb')
+    X = pickle.load(f_x)
+
 #X = np.matrix(X)
-Y = idx2numpy.convert_from_file("train-labels-idx1-ubyte")
 X_test = X[50000:]
-Y_test = Y[50000:]
 X = X[:50000]
-Y = Y[:50000]
+
+#make pair with X and Y
+
+print X.shape
 
 sizes=[X.shape[1],100]
 
@@ -30,7 +49,7 @@ class rbm:
         print 'rbm init ,sizes:',sizes,', numepochs:',numepochs
         self.W=np.matrix(np.zeros(sizes,'float32'))
         self.vW=np.matrix(np.zeros(sizes,'float32'))
-        
+
         self.b=np.matrix(np.zeros(sizes[0],'float32'))
         self.vb=np.matrix(np.zeros(sizes[0],'float32'))
 
@@ -39,30 +58,30 @@ class rbm:
 
         self.learning_rate = learning_rate
         self.numepochs = numepochs
-	
+
         self.v1=self.b
-	self.v2=self.b
-	self.h1=self.c
-	self.h2=self.c
-	self.c1=self.W
-	self.c2=self.W
-    
+        self.v2=self.b
+        self.h1=self.c
+        self.h2=self.c
+        self.c1=self.W
+        self.c2=self.W
+
     def sigmrnd(self,X):
         return np.float32(1./(1.+np.exp(-X)) > np.random.random(X.shape))
 
-    
+
     def train(self,X):
         print 'begin train , X.shape',X.shape,'numepochs:',self.numepochs
         for i in range(self.numepochs):
-	    err = 0
+            err = 0
             for v in X:
                 self.v1 = np.matrix(v)
-                
+
                 self.h1 = self.sigmrnd(self.c+np.dot(self.v1,self.W))
                 self.v2 = self.sigmrnd(self.b+np.dot(self.h1,self.W.T))
                 self.h2 = self.sigmrnd(self.c+np.dot(self.v2,self.W))
 
-		
+
                 self.c1 = np.dot(self.h1.T,self.v1).T
                 self.c2 = np.dot(self.h2.T,self.v2).T
 
@@ -73,144 +92,76 @@ class rbm:
                 self.W = self.W + self.vW
                 self.b = self.b + self.vb
                 self.c = self.c + self.vc
-		
-		err = err + np.linalg.norm(self.v1-self.v2)
+
+                err = err + np.linalg.norm(self.v1-self.v2)
             print "err :",err
         return self.W,self.b,self.c
-    def predict(self,X):
-        m,n = X.shape
-        return self.sigmrnd(np.dot(np.ones((m,1)),self.c)+np.dot(X,self.W)) 
-        
-#rbm1=rbm()
-#rbm1.train(X)
-#print rbm1.predict(X)
 
-#picklestring = pickle.dumps(rbm1)
-#f=open('rbm1','w')
-#f.write(picklestring)
+    def v2h(self,v,k = True):
+        if len(v.shape) == 2:
+             ret = np.dot(np.ones([v.shape[0],1],'float32') ,self.c) + np.dot(v,self.W)
+        else:
+            ret = self.c + np.dot(v, self.W)
 
-#f=open('rbm1','r')
-#rbm1=pickle.load(f)
-#f.close()
+        if(k):
+            return self.sigmrnd(ret)
+        else:
+            return ret
+
+    def h2v(self,h,k = True):
+        if len(h.shape) == 2:
+            ret = np.dot(np.ones([h.shape[0],1],'float32'), self.b) + np.dot(h, self.W.T)
+        else:
+            ret = self.b+np.dot(h, self.W.T)
+
+        if(k):
+            return self.sigmrnd(ret)
+        else:
+            return ret
 
 
 class dbn:
     def __init__(self,sizes = [],learning_rate = 0.01,numepochs = 1):
-        
-        print 'dbn init ,sizes:',sizes,', numepochs:',numepochs        
+        print 'dbn init ,sizes:',sizes,', numepochs:',numepochs
         self.sizes = sizes
         self.rbms = []
         self.learning_rate = learning_rate
         self.numepochs = numepochs
-        self.X = []        
 
         for i in range(len(self.sizes)-1):
-            self.rbms.append(rbm(sizes[i:i+2],0.01,self.numepochs))
-     
+            self.rbms.append(rbm(sizes[i:i+2],self.learning_rate,self.numepochs))
 
     def train(self,X):
-        self.X.append(X)
         #for i in range(self.numepochs):
-	for j in range(len(self.sizes)-1):
-            self.rbms[j].train(self.X[j])
-            self.X.append(self.rbms[j].predict(self.X[j]))
+      for j in range(len(self.sizes)-1):
+            self.rbms[j].train(X)
+            X = self.rbms[j].v2h(X)
 
-    def predict(self,X):
+    def v2h(self,X):
         for j in range(len(self.sizes)-1):
-            X = self.rbms[j].predict(X)
+            X = self.rbms[j].v2h(X)
         return X
 
-sizes = [X.shape[1],200,100]
-dbn1 = dbn(sizes,0.01,4)
-dbn1.train(X)
+    def h2v(self,X):
+        for j in range(len(self.sizes)-1):
+            X = self.rbms[len(self.sizes)-j-1].h2v(X);
+        return X
 
-def sigmoid(X):
-     return 1.0/(1.0+np.exp(-X))
-
-def gradAscent(data,labels,label):
-    m,n=data.shape
-    alpha = 0.01
-    weights = np.ones((n,1))
-    #numepochs = 1
-        
-    #for k in range(numepochs):
-    #    h=sigmoid(data*weights)
-    #    err = (labels-h)
-    #    weights = weights + alpha*data.T*err
-    print 'gradAscent begin'
-    numiter=1
-    for j in range(numiter):
-        print "numiter:", j
-        dataIndex=range(m)
-        for i in range(m):
-            alpha = 4.0/(1.0+j+i)+0.01
-            randIndex = int(np.random.uniform(0,len(dataIndex)))
-            h = sigmoid(sum(data[randIndex]*weights))
-            err = float(labels[randIndex]==label) - h
-            weights = weights + alpha * (data[randIndex].T) * err
-            del(dataIndex[randIndex])
-    return weights
+    def predict(self, X):
+        return self.h2v(self.v2h(X))
 
 
+size_test = [X.shape[1], 100]
+rbm1=rbm(size_test, 0.01, 10)
+rbm1.train(X)
 
-#weights = gradAscent(rbm1.predict(X),Y,7)
-weights=[]
-for i in range(10):
-    weights.append(gradAscent(dbn1.predict(X),Y,i))
-
-
-#for k in range(10):
-#    j = 0
-#    sum1 = 0
-#    sum2 = 0
-#    for i in dbn1.predict(X)*weights[k]:
-#        pre, = i
-#        if Y[j] == k :
-#            sum1 = sum1 + 1
-#            if pre > 0:
-#                sum2 = sum2 + 1
-#        j = j + 1
-#    print float(sum2)/float(sum1),"  ",sum1,"/",sum2
-
-def test():
-    sum1 = [0,0,0,0,0,0,0,0,0,0]
-    sum2 = [0,0,0,0,0,0,0,0,0,0]
-    index = 0
-    for i in dbn1.predict(X_test):
-        temppre = -1000
-        tempY = -1000
-        for j in range(10):
-            pre = i*weights[j]
-            if pre > temppre:
-                temppre = pre
-                tempY = j
-        if tempY==Y_test[index]:
-            sum1[Y_test[index]]=sum1[Y_test[index]]+1    
-        sum2[Y_test[index]]=sum2[Y_test[index]]+1
-        index = index + 1
-    for i in range(10):
-        print float(sum1[i])/float(sum2[i]),' , ',sum1[i],'/',sum2[i]
-
-
-test()
-
-im=Image.open('3.bmp')
+im=Image.open('2.bmp')
 t=np.array(im)
 tt=[]
 for i in t:
-	for j in i:
-		tt.append(j)
-target = []
-target.append(tt)
-target = np.asarray(target,'float32')
-target = target/255.0  # 0-1 scaling
+    for j in i:
+        tt.append(j/255.0)
+for k in range(10):
+        tt.append(0);
 
-temppre = -100
-tempY = -100
-
-for j in range(10):
-    pre = dbn1.predict(target)*weights[j]
-    if pre > temppre:
-        temppre = pre
-        tempY = j
-        print tempY
+print rbm1.h2v(rbm1.v2h(np.asarray(tt)),False)[-10:]
